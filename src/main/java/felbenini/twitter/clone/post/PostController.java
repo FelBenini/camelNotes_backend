f018814eb5d66018e46b27837272a989b294e263
FelBenini <felbenini@gmail.com>
2023-10-03 21:07:17 -0300

    Refactoring and cleaning the code

--- src/main/java/felbenini/twitter/clone/post/PostController.java
+++ src/main/java/felbenini/twitter/clone/post/PostController.java
@@ -1,43 +1,22 @@
 package felbenini.twitter.clone.post;
 
-import felbenini.twitter.clone.infra.security.TokenService;
-import felbenini.twitter.clone.profile.Profile;
-import felbenini.twitter.clone.profile.ProfileRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/post")
 public class PostController {
   @Autowired
-  private PostRepository postRepository;
-  @Autowired
-  private ProfileRepository profileRepository;
-  @Autowired
-  private TokenService tokenService;
+  private PostService postService;
 
   @PostMapping
   public ResponseEntity createPost(@RequestBody @Valid PostRequestDTO data, @RequestHeader(value = "Authorization") String authHeader) {
-    String username = tokenService.extractUsername(authHeader.replace("Bearer ", ""));
-    Profile profile = profileRepository.findByUsername(username);
-    if (profile == null) return ResponseEntity.notFound().build();
-    Post post = new Post(data.getContent(), profile, PostType.POST);
-    postRepository.save(post);
-    return ResponseEntity.ok(post);
+    return postService.createPost(data, authHeader);
   }
-
   @GetMapping("/{username}")
   public ResponseEntity getPostsByUser(@PathVariable("username") String username, @RequestParam(value = "page", required = false) Integer page) {
-    if (page == null) page = 1;
-    Profile profile = profileRepository.findByUsername(username);
-    if (profile == null) return ResponseEntity.notFound().build();
-    Page<Post> posts = postRepository.findByProfile_Username(username, PageRequest.of(page - 1, 15));
-    return ResponseEntity.ok(posts);
+    return this.postService.getPostsFromUser(username, page);
   }
 }
---
+++ src/main/java/felbenini/twitter/clone/post/PostService.java
@@ -0,0 +1,38 @@
+package felbenini.twitter.clone.post;
+
+import felbenini.twitter.clone.infra.security.TokenService;
+import felbenini.twitter.clone.profile.Profile;
+import felbenini.twitter.clone.profile.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+  @Autowired
+  private ProfileRepository profileRepository;
+  @Autowired
+  private PostRepository postRepository;
+  @Autowired
+  private TokenService tokenService;
+
+  public ResponseEntity getPostsFromUser(String username, Integer page) {
+    if (page == null) page = 1;
+    Profile profile = profileRepository.findByUsername(username);
+    if (profile == null) return ResponseEntity.notFound().build();
+    Page<Post> posts = postRepository.findByProfile_Username(username, PageRequest.of(page - 1, 15));
+    Page<PostResponseDTO> postsResponse = posts.map(PostResponseDTO::new);
+    return ResponseEntity.ok(postsResponse);
+  }
+
+  public ResponseEntity createPost(PostRequestDTO data, String token) {
+    String username = tokenService.extractUsername(token.replace("Bearer ", ""));
+    Profile profile = profileRepository.findByUsername(username);
+    if (profile == null) return ResponseEntity.notFound().build();
+    Post post = new Post(data.getContent(), profile, PostType.POST);
+    postRepository.save(post);
+    return ResponseEntity.ok(new PostResponseDTO(post));
+  }
+}
